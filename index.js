1b58e71f9cae79d3e666cbeac3cf8cf54a5773dc
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-06-23 15:55:32 -0400

    committed

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const buttonGrab = document.getElementById("button")
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
 function addingEventListener() {
+    buttonGrab.addEventListener("click", clickAlert)
 }
